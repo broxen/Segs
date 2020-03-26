c96590d0f5d4f42e5fcc816803f29a62d19f7569
broxen <paul@broxen.net>
2020-03-25 17:13:08 -0700

    Remove the test numbers I added to fix the weird duplicate case value error I was getting

--- Projects/CoX/Common/Messages/FriendshipService/FriendHandlerEvents.h
+++ Projects/CoX/Common/Messages/FriendshipService/FriendHandlerEvents.h
@@ -16,7 +16,7 @@ namespace SEGSEvents
 
 enum FriendHandlerEventTypes : uint32_t
 {
-    evFriendConnectedMessage = Internal_EventTypes::ID_LAST_Internal_EventTypes+101,
+    evFriendConnectedMessage = Internal_EventTypes::ID_LAST_Internal_EventTypes,
     evFriendDisconnectedMessage,
     evSendFriendListMessage,
     evSendNotifyFriendMessage,
